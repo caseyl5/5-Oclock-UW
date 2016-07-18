4b61f44f1e144368c6a3841d611014d3dba2467d
lheston <loomisheston@hotmail.com>
2016-07-17 20:00:13 -0700

    udpated partials

--- js/app.js
+++ js/app.js
@@ -2,7 +2,43 @@
 
 var myApp = angular.module('HourApp', ['firebase']);
 
-myApp.controller('HourCtrl', ['$scope', function($scope) {
+//configure routes
+myApp.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+	$stateProvider
+		.state('home', {
+			url: '/home',
+			templateUrl: 'partials/home.html',
+			controller: 'HourCtrl'
+		})
+        .state('signUp', {
+			url: '/signUp',
+			templateUrl: 'partials/signUp.html',
+			controller: 'HourCtrl'
+		})
+        .state('locator', {
+			url: '/locator',
+			templateUrl: 'partials/locator.html',
+			controller: 'HourCtrl'
+		})
+        .state('blog', {
+			url: '/blog',
+			templateUrl: 'partials/blog.html',
+			controller: 'HourCtrl'
+		})
+        .state('favorites', {
+			url: '/favorites',
+			templateUrl: 'partials/favorites.html',
+			controller: 'HourCtrl'
+		})
+
+
+
+
+		$urlRouterProvider.otherwise('/home');
+}]);
+
+
+myApp.controller('HourCtrl', ['$scope', '$stateProvider', function($scope, $stateProvider) {
     
 
 }]);
